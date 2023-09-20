b98a51997fae441282f5c49120d13475533aef81
dat.s.nguyen <dat.s.snguyen@taptap.com.vn>
2023-09-20 14:36:13 +0700

    handle filter on Category page

---
+++ app/(routes)/category/[categoryId]/components/filter.tsx
@@ -0,0 +1,71 @@
+"use client"
+
+import {Color, Size} from "@/types";
+import {useRouter, useSearchParams} from "next/navigation";
+import * as querystring from "querystring";
+import Button from "@/components/ui/button";
+import {cn} from "@/lib/utils";
+
+interface FilterProps {
+	data: (Size | Color)[]
+	name: string
+	valueKey: string
+}
+
+const Filter: React.FC<FilterProps> = ({
+	data,
+	name,
+	valueKey
+}) => {
+	const searchParams = useSearchParams();
+	const router = useRouter();
+
+	const selectedValue = searchParams.get(valueKey);
+
+	const onClick = (id: string) => {
+		const current = querystring.parse(searchParams.toString())
+
+		const query = {
+			...current,
+			[valueKey]: id
+		}
+
+		if (current[valueKey] === id) {
+			//@ts-ignore
+			query[valueKey] = null;
+		}
+
+		const newQuery = querystring.stringify({
+			...query
+		})
+
+		router.push(`${window.location.pathname}?${newQuery}`)
+
+
+	}
+	return (
+		<div className="mb-8">
+			<h3 className="text-lg font-semibold">
+				{name}
+			</h3>
+			<hr className="my-4" />
+			<div className="flex flex-wrap gap-2">
+				{data.map((filter) => (
+					<div key={filter.id} className="flex items-center">
+						<Button
+							className={cn(
+								"rounded-md text-sm text-gray-800 p-2 bg-white border border-gray-300",
+								selectedValue === filter.id && "bg-black text-white"
+							)}
+							onClick={() => onClick(filter.id)}
+						>
+							{filter.name}
+						</Button>
+					</div>
+				))}
+			</div>
+		</div>
+	)
+}
+
+export default Filter;
---
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -0,0 +1,64 @@
+import getProducts from "@/actions/get-products";
+import getSizes from "@/actions/get-sizes";
+import getColors from "@/actions/get-colors";
+import getCategory from "@/actions/get-category";
+import Container from "@/components/container";
+import Billboard from "@/components/billboard";
+import Filter from "@/app/(routes)/category/[categoryId]/components/filter";
+import NoResults from "@/components/ui/no-results";
+import ProductCard from "@/components/ui/product-card";
+import MobileFilters from "@/app/(routes)/category/[categoryId]/components/mobile-filters";
+
+interface CategoryPageProps {
+	params: {
+		categoryId: string;
+	},
+	searchParams: {
+		colorId: string;
+		sizeId: string;
+	}
+}
+
+export const revalidate = 0
+const CategoryPage: React.FC<CategoryPageProps> = async ({
+															 params,
+															 searchParams,
+														 }) => {
+	const products = await getProducts({
+		categoryId: params.categoryId,
+		sizeId: searchParams.sizeId,
+		colorId: searchParams.colorId,
+	})
+
+	const sizes = await getSizes()
+	const colors = await getColors()
+	const category = await getCategory(params.categoryId)
+
+	return (
+		<div className="bg-white">
+			<Container>
+				<Billboard data={category.billboard}/>
+				<div className="px-4 sm:px-6 lg:px-8 pb-24">
+					<div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+						<MobileFilters sizes={sizes} colors={colors} />
+						<div className="hidden lg:block">
+							<Filter data={sizes} name="Sizes" valueKey="sizeId"/>
+							<Filter data={colors} name="Colors" valueKey="colorId"/>
+						</div>
+						<div className="mt-6 lg:col-span-4 lg:mt-0">
+							{products.length === 0 && <NoResults />}
+							<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+								{products.map(item => (
+									<ProductCard data={item} key={item.id} />
+								))}
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</Container>
+		</div>
+	)
+}
+
+export default CategoryPage
